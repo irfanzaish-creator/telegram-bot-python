744a4cc778efa724f9b881495a72e49384101c63
irfanzaish-creator <irfanzaish@gmail.com>
2026-02-02 19:52:21 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,8 @@
+def greet(name):
+    return f"Hello, {name}! Welcome to Python 🚀"
+
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    message = greet(user_name)
+    print(message)
